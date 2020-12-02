5271fa908e688d74b6b1cf5c9a1621d75028d4a8
Ran Wang <randombazinga@gmail.com>
2020-12-02 08:19:27 -0800

    add script

---
+++ build-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in *.org; do cp -- "$f" "ori-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_1-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_2-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_3-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_4-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_5-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_6-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_7-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_8-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_9-$f"; done
+for f in ori-*.org; do cp -- "$f" "group_10-$f"; done
